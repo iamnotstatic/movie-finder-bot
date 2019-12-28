040a61c8d5d13844747604d3aa477a8a2a9b9ad0
Abdul-fatai <staticdev20046@gmail.com>
2019-12-28 02:18:18 +0100

    made some code clean up

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = "856665736:AAGssvnrur4kcmCYt-VgzBgTVQgp4Oa1mDU";
+const token = process.env.BOT_APIKEY;
 let bot = new TelegramBot(token, { polling: true });
 const request = require('request');
 
 bot.onText(/\/movie (.+)/, (msg, match) => {
     let movie = match[1];
     let chatId = msg.chat.id;
-    request(`http://www.omdbapi.com/?apiKey=4664d79d&t=${movie}`, (error, response, body) => {
+    request(`http://www.omdbapi.com/?apiKey=${process.env.OMDAPIKEY}&t=${movie}`, (error, response, body) => {
         if (!error && response.statusCode == 200) {
             bot.sendMessage(chatId, '_Looking for _' + movie + '...', { parse_mode: 'Markdown' })
                 .then((msg) => {
